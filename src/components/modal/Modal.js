f11b538f2f7c26da406acc9e27a8470deda94aa5
ashley.fernandes <ashley.fernandes@upstox.com>
2022-12-31 21:55:48 +0530

    add modal

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,3 +1,28 @@
 export class Modal {
-    
+    constructor(id) {
+        this.modal = document.getElementById(id);
+        this.modal.addEventListener('click', (e) => {
+            if (e.target.parentElement.classList.contains('modal-container')) {
+                this.close();
+            }
+        });
+        this.modal.dataset.modalHidden = "true";
+        this.modal.querySelectorAll('[data-close]').forEach(element => {
+            element.addEventListener('click', () => this.close());
+        });
+    }
+
+    open() {
+        this.modal.style.display = 'block';
+        this.modal.dataset.modalHidden = "false";
+    }
+
+    close() {
+        const modal = this.modal;
+        this.modal.dataset.modalHidden = "true";
+        this.modal.addEventListener('animationend', function handler() {
+            modal.style.display = 'none';
+            modal.removeEventListener('animationend', handler, false);
+        }, false);
+    }
 }
--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -3,4 +3,8 @@ import '../../styles.css';
 import './styles.css';
 
 
-const modal = new Modal('');
+const modal = new Modal('modal-1');
+
+document.getElementById('modal-open').addEventListener('click', function() {
+    modal.open();
+});
