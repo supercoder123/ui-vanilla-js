504459c9c9f1b882bd100ba60e4b62e0e7f9fecf
ashley.fernandes <ashley.fernandes@upstox.com>
2023-01-02 21:20:07 +0530

    update home page

--- src/components/star-rating/index.js
+++ src/components/star-rating/index.js
@@ -3,7 +3,7 @@ import '../../styles.css';
 import './styles.css';
 
 const rating = new StarRating('app', {
-    numberOfStars: 15,
+    numberOfStars: 5,
     onChange: (rating) => {
         document.getElementById('rating').innerText = `You have received ${rating} stars`;
     }
